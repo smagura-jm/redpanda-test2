b71e22356b3d703a87a1129f270546201d8629b9
Sam Magura <smagura@jminsure.com>
2025-03-21 10:53:53 -0400

    wip

---
+++ src/consumer.ts
@@ -0,0 +1,36 @@
+import { Kafka } from 'kafkajs';
+import { v4 as uuidv4 } from 'uuid';
+import { PosDataReceivedEvent } from './events';
+
+const kafka = new Kafka({
+  clientId: 'CustomerService',
+  brokers: ['localhost:19092'],
+});
+
+const consumer = kafka.consumer({ groupId: uuidv4() }); // we need a unique groupId I'll explain down
+
+const handlePosDataReceived = (event: PosDataReceivedEvent) => {
+  console.log(
+    `Hello customer ${event.customer.firstName} ${event.customer.lastName}.`,
+  );
+};
+
+const subscribe = async () => {
+  await consumer.subscribe({ topic: 'PosDataReceived' }).then(() =>
+    consumer.run({
+      eachMessage: async ({ message }) => {
+        const event = JSON.parse((message.value as Buffer).toString());
+        handlePosDataReceived(event);
+      },
+    }),
+  );
+};
+
+export const consumerMain = async () => {
+  await consumer.connect();
+  await subscribe();
+};
+
+export const consumerDisconnect = async () => {
+  await consumer.disconnect();
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { consumerDisconnect, consumerMain } from './consumer';
 import { producerMain } from './producer';
 
 const main = async () => {
+  await consumerMain();
   await producerMain();
+  await consumerDisconnect();
 };
 
 main().catch(console.error);
